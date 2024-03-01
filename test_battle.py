7626c5a1d7111d74dddb28e3be2ccd8d806599f4
Fini <enfinity08@gmail.com>
2024-03-01 00:57:50 -0500

    Tests testing added module check and rearranged order

--- test_battle.py
+++ test_battle.py
@@ -1,5 +1,12 @@
+from rps_functions import battle
+
+def test_module_and_function_name():
+    from rps_functions import battle
+    assert True
+
 def test_players_are_dicts():
+    from rps_functions import battle
     player1 = {}
     player2 = {}
     assert(player1, dict)
-#test
+
