1a8ee1d6743ae69072ed34f9877dcfd3a32fbd93
Fini <enfinity08@gmail.com>
2024-03-02 01:07:22 -0500

    created test for outcomes and murdered meme test function about player dicts

--- test_battle.py
+++ test_battle.py
@@ -1,12 +1,40 @@
+import pytest
+
 from rps_functions import battle
 
 def test_module_and_function_name():
     from rps_functions import battle
     assert True
 
-def test_players_are_dicts():
+def test_empty_players():
+    player1 = {}
+    player2 = {}
+    with pytest.raises(ValueError):
+        battle(player1,player2)
+def test_outcome_results():
     from rps_functions import battle
-    player1 = {"name":"name", "option1":"rock", "option1":"paper", "option1":"scissors"}
-    player2 = {"name":"name", "option1":"rock", "option1":"paper", "option1":"scissors"}
-    assert isinstance(player1, dict)
-
+    player1 = {"choice": "null", "discord_id": 123456, "username": "Cherub Fish"}
+    player2 = {"choice": "null", "discord_id": 654321, "username": "Jawfish"}
+    choices = ["rock", "paper", "scissors"]
+    for x in choices:
+        player1["choice"] = x
+        for y in choices:
+            player2["choice"] = y
+            outcome = battle(player1, player2)
+            if x == y:
+                assert outcome is None  # tie cases
+            if x == "rock":
+                if y == "paper":
+                    assert outcome == player2
+                if y == "scissors":
+                    assert outcome == player1
+            if x == "paper":
+                if y == "scissors":
+                    assert outcome == player2
+                if y == "rock":
+                    assert outcome == player1
+            if x == "scissors":
+                if y == "rock":
+                    assert outcome == player2
+                if y == "paper":
+                    assert outcome == player1
