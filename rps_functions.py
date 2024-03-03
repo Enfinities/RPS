c55c980373ee83be12c032d55a0ce6ce68d192a3
Fini <enfinity08@gmail.com>
2024-03-02 23:25:13 -0500

    update

--- rps_functions.py
+++ rps_functions.py
@@ -1,3 +1,26 @@
-def battle():
-    player1 = {}
-    player2 = {}
+def battle(player1,player2):
+    player1 = {"choice": "rock", "discord_id": 123456, "username": "Cherub Fish"}
+    player2 = {"choice": "paper", "discord_id": 654321, "username": "Jawfish"}
+    choice = ["rock", "paper", "scissors"]
+
+    if player1["choice"] == "rock":
+        if player2["choice"] =="scissors":
+            return player1
+        if player2["choice"] =="paper":
+            return player2
+        if player2["choice"] =="rock":
+            return None
+    if player1[choice] == "paper":
+        if player2["choice"] =="rock":
+            return player1
+        if player2["choice"] =="scissors":
+            return player2
+        if player2["choice"] =="paper":
+            return None
+    if player1[choice] == "scissors":
+        if player2["choice"] =="paper":
+            return player1
+        if player2["choice"] =="rock":
+            return player2
+        if player2["choice"] =="scissors":
+            return None
