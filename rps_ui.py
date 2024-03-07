d77b5133fc830ce9118be51ed77f3eef51d17d39
Fini <enfinity08@gmail.com>
2024-03-07 01:27:57 -0500

    Started ui work, updated reqs

--- rps_ui.py
+++ rps_ui.py
@@ -1,16 +1,22 @@
 import interactions
 from decouple import config
-from interactions import slash_command, SlashContext
+from interactions import slash_command, SlashContext, SlashCommand
 
 Var_1 = config('VAR1')
 Var_2 = config('VAR2')
 BOT_TOKEN = Var_1 + Var_2
 bot = interactions.Client(token=BOT_TOKEN)
+base = SlashCommand(
+    name="test",
+    description="Commands involving logs"
+)
 
-
-@slash_command(name="my_command", description="My first command :)")
+@slash_command(name="rps", description="rock, paper , scissors")
 async def my_command_function(ctx: SlashContext):
     await ctx.send("Hello World")
+
+
+
 bot.start()
 if __name__ == "__main__":
     bot.start()
