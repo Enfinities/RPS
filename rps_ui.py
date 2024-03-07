ebc81c3b918e11388348dd7f33a26ec22f1551c9
Fini <enfinity08@gmail.com>
2024-03-07 02:37:43 -0500

    working ui

--- rps_ui.py
+++ rps_ui.py
@@ -1,6 +1,6 @@
 import interactions
 from decouple import config
-from interactions import slash_command, SlashContext, SlashCommand
+from interactions import slash_command, SlashContext, SlashCommand, slash_option, SlashCommandChoice, OptionType
 
 Var_1 = config('VAR1')
 Var_2 = config('VAR2')
@@ -12,10 +12,21 @@ base = SlashCommand(
 )
 
 @slash_command(name="rps", description="rock, paper , scissors")
-async def my_command_function(ctx: SlashContext):
-    await ctx.send("Hello World")
+@slash_option(
+    name="rps_option",
+    description="RPS Option",
+    required=True,
+    opt_type=OptionType.STRING,
+    choices=[
+        SlashCommandChoice(name="rock", value="rock"),
+        SlashCommandChoice(name="paper", value="paper"),
+        SlashCommandChoice(name="scissors", value="scissors")
+    ]
 
+)
 
+async def my_command_function(ctx: SlashContext,rps_option):
+    await ctx.send(rps_option, ephemeral=True)
 
 bot.start()
 if __name__ == "__main__":
